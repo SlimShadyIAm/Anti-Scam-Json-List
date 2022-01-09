40c9633c11d740c0ac815323f745ad08b8009e3a
Lillie <91358136+LillieWeeb001@users.noreply.github.com>
2022-01-08 21:01:19 -0500

    Add duplicate check to python script, update list

--- antiscam.py
+++ antiscam.py
@@ -13,3 +13,13 @@ f.write('\n\t"scamideviceunlockurls": ')
 f.write(str(scamideviceunlockurls).replace("'", '"').replace("[", "[\n\t\t").replace("]", "\n\t]").replace(", ", ",\n\t\t"))
 f.write('\n}')
 f.close()
+
+for i in range(len(scamjburls)):
+    for j in range(i + 1, len(scamjburls)):
+        if scamjburls[i] == scamjburls[j]:
+            print("Duplicate:", scamjburls[i])
+
+for i in range(len(scamideviceunlockurls)):
+    for j in range(i + 1, len(scamideviceunlockurls)):
+        if scamideviceunlockurls[i] == scamideviceunlockurls[j]:
+            print("Duplicate:", scamideviceunlockurls[i])
