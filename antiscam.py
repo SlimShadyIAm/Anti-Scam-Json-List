370faf2313d72af92e8a358cc67fcdf2eff19630
Lillie <91358136+LillieWeeb001@users.noreply.github.com>
2022-01-17 17:30:45 -0500

    Update list and include discord scams

--- antiscam.py
+++ antiscam.py
@@ -4,13 +4,16 @@ antiscamjson = open('antiscam.json')
 data = json.load(antiscamjson)
 scamjburls = sorted(data["scamjburls"])
 scamideviceunlockurls = sorted(data["scamideviceunlockurls"])
+scamdiscordurls = sorted(data["scamdiscordurls"])
 antiscamjson.close()
 
 f = open("antiscam.json", "w")
 f.write('{\n\t"scamjburls": ')
 f.write(str(scamjburls).replace("'", '"').replace("[", "[\n\t\t").replace("]", "\n\t],").replace(", ", ",\n\t\t"))
 f.write('\n\t"scamideviceunlockurls": ')
-f.write(str(scamideviceunlockurls).replace("'", '"').replace("[", "[\n\t\t").replace("]", "\n\t]").replace(", ", ",\n\t\t"))
+f.write(str(scamideviceunlockurls).replace("'", '"').replace("[", "[\n\t\t").replace("]", "\n\t],").replace(", ", ",\n\t\t"))
+f.write('\n\t"scamdiscordurls": ')
+f.write(str(scamdiscordurls).replace("'", '"').replace("[", "[\n\t\t").replace("]", "\n\t]").replace(", ", ",\n\t\t"))
 f.write('\n}')
 f.close()
 
@@ -24,6 +27,11 @@ for i in scamideviceunlockurls:
         if z.isupper():
             print("Capital:", i)
 
+for i in scamdiscordurls:
+    for z in i:
+        if z.isupper():
+            print("Capital:", i)
+
 for i in range(len(scamjburls)):
     for j in range(i + 1, len(scamjburls)):
         if scamjburls[i] == scamjburls[j]:
@@ -33,5 +41,10 @@ for i in range(len(scamideviceunlockurls)):
     for j in range(i + 1, len(scamideviceunlockurls)):
         if scamideviceunlockurls[i] == scamideviceunlockurls[j]:
             print("Duplicate:", scamideviceunlockurls[i])
+
+for i in range(len(scamdiscordurls)):
+    for j in range(i + 1, len(scamdiscordurls)):
+        if scamdiscordurls[i] == scamdiscordurls[j]:
+            print("Duplicate:", scamdiscordurls[i])
 
 print("Done")
